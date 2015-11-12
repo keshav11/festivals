a1a53c131ed51a0cafe04ec86eb261fe7764a103
Keshav Gupta <keshav1032@gmail.com>
2015-11-12 21:29:55 +0530

    Added server

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var Twit = require('twit');
-var schedule = require('node-schedule');
-
 var twitter = new Twit({
     consumer_key:         'consumer_key',
     consumer_secret:      'consumer_secret',
     access_token:         'access_token',
     access_token_secret:  'access_token_secret'
+});
+
+var schedule = require('node-schedule');
+require('date-utils');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+// public folder
+app.use(express.static(__dirname + '/public'));
+
+// views
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.send("Hello");
 });
 
 var festivals2015 = [
@@ -17,7 +32,7 @@ var festivals2015 = [
   },
 
   {
-    date: new Date('25-12-2015') ,
+    date: new Date('12-25-2015') ,
     name: 'Christmas',
     wiki: 'https://en.wikipedia.org/wiki/Christmas'
   }
@@ -26,13 +41,25 @@ var festivals2015 = [
 
 festivals2015.forEach(function (elem)
 {
+
+  if(Date.compare(elem.date, Date.today()) > 0)
+  {
+    console.log(Date.compare(elem.date, Date.today()));
     var j = schedule.scheduleJob(elem.date, function(){
-          twitter.post('statuses/update',
+
+          T.post('statuses/update',
             {
               status: 'Happy ' + elem.name + '!\n\n' + elem.wiki
+
             },
+
             function(err, data, response) {
                           console.log(data)
             });
       }
-)});
+
+)}});
+
+app.listen(app.get('port'), function() {
+  console.log('App is running', app.get('port'));
+});
